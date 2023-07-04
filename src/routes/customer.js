145eb2ac8241ba48ebd053395c18672e862d499a
dungngoo <ngodung06vn@gmail.com>
2023-07-04 22:39:04 +0700

    zxczxc

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const customerController = require("../app/controllers/customerController");
+const customerController = require("../app/controllers/customerController.js");
 
 /**
  * @swagger
