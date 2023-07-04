f2108a52ffae82d1b8bb9c08e661e36cab38261b
dungngoo <ngodung06vn@gmail.com>
2023-07-04 23:23:07 +0700

    zxczxczxczxc

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const middlewareController = require("../app/controllers/MiddlewareController");
 const adminController = require("../app/controllers/AdminController");
 
 /**
